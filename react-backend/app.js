879665b8366b19cafa0e3e69bea4f2e9807e4d20
Sumire <eric.huang.nexus@gmail.com>
2021-08-16 15:12:33 +1000

    Revert "moved client(react front end)->react-backend"
    This reverts commit 90492bea7e37ddd932b610f269b00e90dcf272b0.

--- react-backend/app.js
+++ react-backend/app.js
@@ -22,13 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
