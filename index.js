8f98435f205429593039b4266978de8141f7d6df
Sumire <eric.huang.nexus@gmail.com>
2021-08-16 19:57:44 +1000

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
+const port = process.env.PORT || 5001;
+app.listen(port);
+
+console.log(`Password generator listening on ${port}`);
