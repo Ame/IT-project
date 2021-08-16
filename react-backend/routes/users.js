ee287021abb241f853f52bf45c6476f2b5dcadb3
Sumire <eric.huang.nexus@gmail.com>
2021-08-16 17:46:41 +1000

    Heroku react fix

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([{
+    id: 1,
+    username: "user1"
+  }, {
+    id: 2,
+    username: "user2"
+  }]);
 });
 
 module.exports = router;
