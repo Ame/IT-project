0911eded028e19215250a32ecc4495d01b1f1b69
Irina M <40161275+imoskvit@users.noreply.github.com>
2021-09-28 01:33:00 +1000

    Tweaks and space for filter tags

--- client/src/components/Contacts/Search.js
+++ client/src/components/Contacts/Search.js
@@ -5,11 +5,13 @@ const Search = ({ searchQuery, setSearchQuery }) => (
     
     <form action="/contacts" method="get">
         <div id="search">
-              <label for="searchInput">Find <i class="fa fa-search"></i>Contacts</label>
               <input 
-                id="searchInput" type="text" placeholder="Search" name="s" 
+                id="searchInput" type="text" placeholder="Search by Name" name="s" 
                 value={searchQuery} onInput={e => setSearchQuery(e.target.value)}/>
               <button type="submit">Search</button>
+        </div>
+        <div className="container" id="tags">
+            <strong>Filter by tags:&nbsp; </strong> list tags here
         </div>
     </form>
 );
