23f0a3f5e8b717963fc61c1279c9330d51456fd3
Sumire <eric.huang.nexus@gmail.com>
2021-09-29 04:56:07 +1000

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ connectDB()
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
+app.options('*', cors());
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
