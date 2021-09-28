24e3c3eb6d6effd0f17e739f96816bfb4aa7e636
abbeylippe <abbey.lippe@gmail.com>
2021-09-28 14:29:59 +1000

    moving private route into the correct folder

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,7 +4,7 @@ export { default as Home } from "./Login/Login";
 export { default as Signup } from "./Signup/Signup";
 export { default as About } from "./About/About";
 export { default as Dashboard} from "./Dashboard/Dashboard";
-export { default as PrivateRoute } from "../PrivateRoute/PrivateRoute"
+export { default as PrivateRoute } from "./PrivateRoute/PrivateRoute"
 export {default as Contacts} from "./Contacts/Contacts";
 export {default as AddContact} from './AddContact/AddContact';
 export {default as Sidebar} from './Sidebar/Sidebar';
