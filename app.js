dafa2e829ae34d095faa45a3df82666f084f1067
Sumire <eric.huang.nexus@gmail.com>
2021-09-29 14:09:21 +1000

    Update app.js

--- app.js
+++ app.js
@@ -19,9 +19,6 @@ app.options('*', cors());
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
 
 
 // logger - only use in dev mode
