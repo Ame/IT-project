8eafb1260fdf2e268c0a67587afdd5b0de977142
Sumire <eric.huang.nexus@gmail.com>
2021-09-30 12:00:15 +1000

    Merge request

--- client/src/App.js
+++ client/src/App.js
@@ -6,34 +6,34 @@ import { Navigation, Footer, Home, Signup, Dashboard, About, PrivateRoute, Conta
 
 function App() {
 
-  return (
-    <div className="App">
-      <Router>
-        <Navigation />
-        
-        <Switch>
-          <Route path="/" exact component={() => <Login />} />
-          <Route path="/signup" exact component={() => <Signup />} />
-          <Route path="/about" exact component={() => <About/>} />
-          <PrivateRoute path="/dashboard" exact component={() => <Dashboard/>}/>
-          <PrivateRoute path="/contacts" exact component={() => <Contacts/>}/>
-          <PrivateRoute path="/addContact" exact component={() => <AddContact/>} />
-          <PrivateRoute path="/editContact" exact component={() => <EditContact/>} />
-          <PrivateRoute path="/editProfile">
-              <div className="row">
-               <div className="col-lg-3">
-                  <Sidebar />
-                </div>
-                <div className = "col-lg-7">
-                  <EditProfile />
-                </div>
-              </div>
-          </PrivateRoute>
-        </Switch>
-        <Footer />
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Navigation />
+
+                <Switch>
+                    <Route path="/" exact component={() => <Home />} />
+                    <Route path="/signup" exact component={() => <Signup />} />
+                    <Route path="/about" exact component={() => <About/>} />
+                    <PrivateRoute path="/dashboard" exact component={() => <Dashboard/>}/>
+                    <PrivateRoute path="/contacts" exact component={() => <Contacts/>}/>
+                    <PrivateRoute path="/addContact" exact component={() => <AddContact/>} />
+                    <PrivateRoute path="/editContact" exact component={() => <EditContact/>} />
+                    <PrivateRoute path="/editProfile">
+                        <div className="row">
+                            <div className="col-lg-3">
+                                <Sidebar />
+                            </div>
+                            <div className = "col-lg-7">
+                                <EditProfile />
+                            </div>
+                        </div>
+                    </PrivateRoute>
+                </Switch>
+                <Footer />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
