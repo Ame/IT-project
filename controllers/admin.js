010658322acb324d074f412ac32eb320722f35ef
Jack Sopher <sopherjack@gmail.com>
2021-10-03 20:17:32 +1100

    Delete and Edit User permissions implemented

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,4 +9,26 @@ module.exports =  class AdminController {
         })
         .catch()
     }
+
+    static async apiDeleteUser(req,res){
+        const email = req.params.email
+        // delete the user with the email
+        User.deleteOne({email:email}).then(user => {
+            res.status(200).json({success:true,user:user})
+        })
+        .catch(err => res.status(400).json({errors:err}))
+    }
+
+    static async apiEditUser(req,res){
+        const email = req.params.email
+        const role = req.body.role
+        // edit the user with the email
+        User.findOneAndUpdate({email:email},{role:role}).then(user => {
+            res.status(200).json({success:true,user:user})
+        })
+        .catch(err => res.status(400).json({errors:err}))
+
+
+    }
+
 }
