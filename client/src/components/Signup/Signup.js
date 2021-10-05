9187366579b03e4db0fe65d1be8f7ce1205263f1
abbeylippe <abbey.lippe@gmail.com>
2021-10-05 14:35:07 +1100

    adding password confirmation validation to sign up form

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,5 +1,5 @@
-import React, { useState, useRef} from "react";
-import {Link} from "react-router-dom";
+import React, { useState, useRef } from "react";
+import { Link, useHistory } from "react-router-dom";
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
@@ -7,48 +7,18 @@ import { isEmail } from "validator";
 
 import AuthService from "../../services/auth.service";
 
-
-export const required = (value) => {
-  if (!value) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        This field is required!
-      </div>
-    );
-  }
-};
-
-export const validEmail = (value) => {
-  if (!isEmail(value)) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        This is not a valid email.
-      </div>
-    );
-  }
-};
-
-
-export const vpassword = (value) => {
-  if (value.length < 6) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        The password must be between 6 and 40 characters.
-      </div>
-    );
-  }
-};
-
 function Signup() {
-
   const form = useRef();
   const checkBtn = useRef();
+  let history = useHistory();
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [successful, setSuccessful] = useState(false);
   const [message, setMessage] = useState("");
+  const [errors, setErrors] = useState({});
 
   const onChangeName = (e) => {
     const name = e.target.value;
@@ -65,36 +35,84 @@ function Signup() {
     setPassword(password);
   };
 
+  const onChangeConfirmPassword = (e) => {
+    const confirmPassword = e.target.value;
+    setConfirmPassword(confirmPassword);
+  };
+
+  const handleValidation = () => {
+    const errors = {};
+    let formIsValid = true;
+
+    if (!name){
+      formIsValid = false;
+      errors["name"] = "This field is required!";
+    }
+
+    if (!email){
+      formIsValid = false;
+      errors["email"] = "This field is required!";
+    }
+
+    if (!isEmail(email)){
+      formIsValid = false;
+      errors["email"] = "Invalid email";
+    }
+
+    if (password.length <= 6){
+      formIsValid = false;
+      errors["password"] = "Password must be greater than 6 character";
+    }
+
+    if (!password){
+      formIsValid = false;
+      errors["password"] = "This field is required!";
+    }
+
+    if (!confirmPassword){
+      formIsValid = false;
+      errors["confirmPassword"] = "This field is required!";
+    }
+
+    if (password !== confirmPassword){
+      formIsValid = false;
+      errors["confirmPassword"] = "Passwords don't match";
+    }
+
+    setErrors(errors);
+    return formIsValid;
+  }
+
   const handleRegister = (e) => {
     e.preventDefault();
 
     setMessage("");
     setSuccessful(false);
 
-    form.current.validateAll();
+    if (handleValidation()){
+      if (checkBtn.current.context._errors.length === 0) {
+        AuthService.register(name, email, password).then(
+          (response) => {
+            setMessage(response.data.message);
+            setSuccessful(true);
+            history.push("/");
+            window.location.reload();
+          },
+          (error) => {
+            const resMessage =
+              (error.response &&
+                error.response.data &&
+                error.response.data.message) ||
+              error.message ||
+              error.toString();
 
-    if (checkBtn.current.context._errors.length === 0) {
-      console.log(name, email, password);
-      AuthService.register(name, email, password).then(
-        (response) => {
-          setMessage(response.data.message);
-          setSuccessful(true);
-        },
-        (error) => {
-          const resMessage =
-            (error.response &&
-              error.response.data &&
-              error.response.data.message) ||
-            error.message ||
-            error.toString();
-
-          setMessage(resMessage);
-          setSuccessful(false);
-        }
-      );
+            setMessage(resMessage);
+            setSuccessful(false);
+          }
+        );
+      }
     }
   };
-
 
   return (
     <div className="signup">
@@ -111,66 +129,91 @@ function Signup() {
             <h1 className="font-weight-light">Signup</h1>
             <p>Start networking with iJane CRM</p>
             <div>
-            <Form onSubmit={handleRegister} ref={form}>
-          {!successful && (
-            <div>
-              <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <Input
-                  type="text"
-                  className="form-control"
-                  name="name"
-                  value={name}
-                  onChange={onChangeName}
-                  validations={[required]}
-                />
-              </div>
+              <Form onSubmit={handleRegister} ref={form}>
+                {!successful && (
+                  <div>
+                    <div className="form-group">
+                      <label htmlFor="name">Name</label>
+                      <Input
+                        type="text"
+                        className="form-control"
+                        name="name"
+                        value={name}
+                        onChange={onChangeName}
+                      />
+                      <span
+                        style={{ color: "red" }}
+                      >
+                        {errors["name"]}
+                      </span>
+                    </div>
 
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <Input
-                  type="text"
-                  className="form-control"
-                  name="email"
-                  value={email}
-                  onChange={onChangeEmail}
-                  validations={[required, validEmail]}
-                />
-              </div>
+                    <div className="form-group">
+                      <label htmlFor="email">Email</label>
+                      <Input
+                        type="text"
+                        className="form-control"
+                        name="email"
+                        value={email}
+                        onChange={onChangeEmail}
+                      />
+                      <span style={{ color: "red" }}>{errors["email"]}</span>
+                    </div>
 
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <Input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  value={password}
-                  onChange={onChangePassword}
-                  validations={[required, vpassword]}
-                />
-              </div>
+                    <div className="form-group">
+                      <label htmlFor="password">Password</label>
+                      <Input
+                        type="password"
+                        className="form-control"
+                        name="password"
+                        value={password}
+                        onChange={onChangePassword}
+                      />
+                      <span style={{ color: "red" }}>{errors["password"]}</span>
+                    </div>
 
-              <div className="form-group">
-                <button className="btn btn-primary btn-block">Sign Up</button>
-              </div>
+                    <div className="form-group">
+                      <label htmlFor="confirmPassword">Confirm Password</label>
+                      <Input
+                        type="password"
+                        className="form-control"
+                        name="confirmPassword"
+                        value={confirmPassword}
+                        onChange={onChangeConfirmPassword}
+                      />
+                      <span style={{ color: "red" }}>
+                        {errors["confirmPassword"]}
+                      </span>
+                    </div>
+
+                    <div className="form-group">
+                      <button className="btn btn-primary btn-block">
+                        Sign Up
+                      </button>
+                    </div>
+                  </div>
+                )}
+
+                {message && (
+                  <div className="form-group">
+                    <div
+                      className={
+                        successful
+                          ? "alert alert-success"
+                          : "alert alert-danger"
+                      }
+                      role="alert"
+                    >
+                      {message}
+                    </div>
+                  </div>
+                )}
+                <CheckButton style={{ display: "none" }} ref={checkBtn} />
+              </Form>
             </div>
-          )}
-
-          {message && (
-            <div className="form-group">
-              <div
-                className={ successful ? "alert alert-success" : "alert alert-danger" }
-                role="alert"
-              >
-                {message}
-              </div>
-            </div>
-          )}
-          <CheckButton style={{ display: "none" }} ref={checkBtn} />
-        </Form>
-            </div>
-            <p>Already have an account? <Link to="/">Log in</Link> here!</p>
-            
+            <p>
+              Already have an account? <Link to="/">Log in</Link> here!
+            </p>
           </div>
         </div>
       </div>
