0fdd3e0e7ba9e28e0e6b479747a612edcc2afc69
Jack Sopher <sopherjack@gmail.com>
2021-10-06 18:50:20 +1100

    Finished user edit profile backend

--- controllers/users.js
+++ controllers/users.js
@@ -60,7 +60,25 @@ static async apiLoginUser(req,res){
             return res.status(400).json({msg: 'Invalid Credientials'});
         }
     })
-}}
+
+    
+}
+
+static async apiEditProfile(req,res){
+    const email = req.body.email
+    const password = req.body.password
+    const id = req.user.id
+    // edit the user with the id 
+    User.findOneAndUpdate({_id:id},{password:password,email:email},{returnNewDocument:true}).then(user => {
+        res.status(200).json({success:true,user:user})
+    })
+    .catch(err => res.status(400).json({errors:err}))
+
+
+}
+
+}
+
 
 // issues a token for the user 
 function issueToken(user) {
