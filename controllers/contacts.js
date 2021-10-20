e01f5ff1812000deadcb98f8962f8efc7ab2ddae
Jack Sopher <sopherjack@gmail.com>
2021-10-20 18:17:50 +1100

    Multiple users, same contact fixed

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -8,7 +8,7 @@ module.exports =  class ContactsController {
         if (!isValid) {
             return res.status(400).json(errors);
         }
-        Contact.findOne({ email: req.body.email , id: req.body.id}).then(contact => {
+        Contact.findOne({ email: req.body.email , user: req.user.id}).then(contact => {
             // Contact already exists
             if (contact) return res.status(400).json({ message: "Contact already exists" });
 
